a4c1b5a02bed4b588410ea1dec429ba579430f98
Sajeeb <sajeebahamed6@gmail.com>
2020-08-15 21:12:15 +0600

    item one and subtotal worked

--- js-add-to-cart/main.js
+++ js-add-to-cart/main.js
@@ -7,13 +7,29 @@ function handleProductChange(product, isIncrease) {
     if (isIncrease == true) {
         productCount = parseFloat(productValue) + 1
         console.log(productCount);
-        // document.getElementById('phone-total').innerText = parseFloat(productCount * phoneTotal)
+        document.getElementById('phone-total').innerText = 1200 * productCount
         document.getElementById('phone-count').value = productCount
+
+
+        const currentPhoneTotal = document.getElementById('phone-total').innerText
+        document.getElementById('total-price').innerText = currentPhoneTotal
+        
+        
+
+        
+
+
+
+
     }
     if(isIncrease == false && productValue >0 ) {
         productCount = parseFloat(productValue) - 1
         console.log(productCount);
+        document.getElementById('phone-total').innerText = phoneTotal - 1200
         document.getElementById('phone-count').value = productCount
+
+        const currentPhoneTotal = document.getElementById('phone-total').innerText
+        document.getElementById('total-price').innerText = phoneTotal - 1200 
     }
 
 }
